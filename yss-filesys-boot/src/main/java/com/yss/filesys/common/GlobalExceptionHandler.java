ad474477ed2fdc1418409bc12158dfe735ae9c67
daomingzhu <zhudaoming@ysstech.com>
2026-04-17 22:46:37 +0800

    fix: raise multipart upload limit

--- yss-filesys-boot/src/main/java/com/yss/filesys/common/GlobalExceptionHandler.java
+++ yss-filesys-boot/src/main/java/com/yss/filesys/common/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,12 @@ public class GlobalExceptionHandler {
                 ? "请求参数不合法"
                 : ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         return ResponseEntity.badRequest().body(ApiResponse.fail(message));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMaxUpload(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(ApiResponse.fail("上传文件过大，请拆分为更小的分片后重试"));
     }
 
     @ExceptionHandler(Exception.class)
